0abb8e4d28488bb88b098cc7a56339c9dd291a96
Beepered <bktrieu@gmail.com>
2024-11-19 10:43:58 -0800

    simple menu

---
+++ src/main.js
@@ -0,0 +1,21 @@
+let config = {
+    type: Phaser.CANVAS,
+    render: {
+        pixelArt: true
+    },
+    physics:{
+        default: "arcade",
+        arcade:{
+            debug:false
+        }
+    },
+    width: 800,
+    height: 500,
+    scene: [Menu]
+}
+
+let game = new Phaser.Game(config);
+let gameHeight = game.config.height
+let gameWidth = game.config.width
+
+let keyUP, keyDOWN
---
+++ src/scenes/Menu.js
@@ -0,0 +1,46 @@
+class Menu extends Phaser.Scene {
+    constructor(){
+        console.log("start menu")
+        super("menuScene")
+    }
+
+    create(){
+        let titleConfig = {
+            fontFamily: "Montserrat",
+            fontSize: "60px",
+            color: "#FF0000",
+            align: "center",
+            padding: {
+                top: 5,
+                bottom: 5
+            },
+        }
+        let textConfig = {
+            fontFamily: "Montserrat",
+            fontSize: "22px",
+            color: "#FFFFFF",
+            align: "center",
+            padding: {
+                top: 5,
+                bottom: 5
+            },
+        }
+        this.add.text(gameWidth / 2, gameHeight / 2.5, "TITLE", titleConfig).setOrigin(0.5)
+        this.add.text(gameWidth / 2, gameHeight / 2, "press UP to PLAY", textConfig).setOrigin(0.5)
+        this.add.text(gameWidth / 2, gameHeight / 1.8, "press DOWN for CREDITS", textConfig).setOrigin(0.5)
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+    }
+
+    update(){
+        if(Phaser.Input.Keyboard.JustDown(keyUP)){
+            console.log("play")
+            //this.scene.start("playScene")
+        }
+        if(Phaser.Input.Keyboard.JustDown(keyDOWN)){
+            console.log("credits")
+            //this.scene.start("creditScene")
+        }
+    }
+
+}
