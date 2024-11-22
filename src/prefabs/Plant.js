4439a263e0b5288af5aeb702b1fe99ec2fe7ff7b
AndrBy1 <abyi@ucsc.edu>
2024-11-21 16:55:43 -0800

    Plant.js modified

--- src/prefabs/Plant.js
+++ src/prefabs/Plant.js
@@ -1,7 +1,6 @@
 class Plant extends Phaser.GameObjects.Sprite{
-    constructor(scene, x, y, type){ //type is a number between 0 - 2
-        const texture = ["red", "pink", "purple"]; //names for texture
-        super(scene, x, y, texture[type]);
+    constructor(scene, x, y, type){
+        super(scene, x, y, type);
         this.growth = 0;
         scene.add.existing(this);
     }
